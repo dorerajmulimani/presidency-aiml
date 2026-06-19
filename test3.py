c07895c75d3d67425579a60bb90d196a7a2e9244
dore <doremulimani22@gamil.com>
2026-06-19 17:15:36 +0530

    api

--- test3.py
+++ test3.py
@@ -1,4 +1,32 @@
-def agree():
-    print("hello")
-    
-agree()
+from fastapi import FastAPI, Header, HTTPException, Depends
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="FastAPI KT",
+    description="This is a test FastAPI application",
+    version="1.0.0"
+)
+
+data = {
+    "user": "dore",
+    "phone": 125252525
+}
+
+def verify_token(token: str = Header(None)):
+    if token != "apoloo565555sds":
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+        )
+
+    return {
+        "user": "Authorized"
+    }
+
+@app.get("/secure-data")
+def secure_data(user=Depends(verify_token)):
+    return {
+        "message": "Access granted",
+        "user": user,
+        "data": data
+    }
